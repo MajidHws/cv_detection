1d122ff494ad2560500ee40db290923207b5ead3
majidhws <majidhws@gmail.com>
2023-03-26 17:37:42 +0300

    post detection

--- pose_detection/PoseDetection.py
+++ pose_detection/PoseDetection.py
@@ -1,19 +1,26 @@
 import cv2
-import mediapipe as mp
 import time
-
+import PoseDetectionModule as pm
+cap = cv2.VideoCapture(0)
 
 pTime = 0
 cTime = 0
-cap = cv2.VideoCapture(0)
+
+detector = pm.poseDetector()
 
 while True:
     success, img = cap.read()
+    img = detector.findPose(img)
+    lmList = detector.findPosition(img, False)
+    if len(lmList):
+        cv2.circle(img, (lmList[14][1], lmList[14][2]), 10, (0,0,0), cv2.FILLED)
+    # print(lmList)
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
     pTime = cTime
+
     cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0,0,0), 3)
 
-    cv2.imshow('Imaeg', img)
+    cv2.imshow('Image', img)
     cv2.waitKey(1)
--- pose_detection/PoseDetectionModule.py
+++ pose_detection/PoseDetectionModule.py
@@ -3,4 +3,71 @@ import mediapipe as mp
 import time
 
 
+class poseDetector():
 
+    def __init__(self, mode=False, upBody=False, smooth=True, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.upBody = upBody
+        self.smooth = smooth
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpDraw = mp.solutions.drawing_utils
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose()
+        # self.pose = self.mpPose.Pose(self.mode, self.upBody, self.smooth, self.detectionCon, self.trackCon)
+
+    def findPose(self, img, draw=True):
+
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.pose.process(imgRGB)
+
+        if self.results.pose_landmarks:
+            if draw:
+                self.mpDraw.draw_landmarks(img, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
+        return img
+
+
+    def findPosition(self, img, draw=True):
+        
+        lmList = []
+        if self.results.pose_landmarks:
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                height, width, channel = img.shape
+                cx, cy = int(lm.x * width), int(lm.y * height)
+                lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 10, (0,0,0), cv2.FILLED)
+        
+        return lmList
+
+
+def main():
+
+    cap = cv2.VideoCapture(0)
+
+    pTime = 0
+    cTime = 0
+
+    detector = poseDetector()
+
+    while True:
+        success, img = cap.read()
+        img = detector.findPose(img)
+        lmList = detector.findPosition(img, False)
+        if len(lmList):
+            cv2.circle(img, (lmList[14][1], lmList[14][2]), 10, (0,0,0), cv2.FILLED)
+        # print(lmList)
+
+        cTime = time.time()
+        fps = 1/(cTime-pTime)
+        pTime = cTime
+
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0,0,0), 3)
+
+        cv2.imshow('Image', img)
+        cv2.waitKey(1)
+
+
+if __name__ == '__main__':
+    main()
